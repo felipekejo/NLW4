1effbee01da4af937fa7e9a12df80cee0bcfa620
Felipe Yui <felipe_m87@yahoo.com.br>
2021-02-28 16:17:26 +1030

    VAlidation Surveys Controller

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -1,29 +1,44 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 import { SurveysRepository } from '../repositories/SurveysRepository';
+
 
 class SurveysController {
   async create(request: Request, response: Response) {
+
+    const schema = yup.object().shape({
+      title: yup.string().required(),
+      description: yup.string().required()
+    })
+
+    try {
+      await schema.validate(request.body, { abortEarly: false })
+    } catch (err) {
+      throw new AppError(err)
+    }
+
     const { title, description } = request.body
 
-    const surveysRepostitory = getCustomRepository(SurveysRepository)
+    const surveysRepository = getCustomRepository(SurveysRepository)
 
-    const survey = surveysRepostitory.create({
+    const survey = surveysRepository.create({
       title,
       description
     })
 
-    await surveysRepostitory.save(survey)
+    await surveysRepository.save(survey)
 
     return response.status(201).json(survey)
   }
 
   async show(request: Request, response: Response) {
-    const surveysRepostitory = getCustomRepository(SurveysRepository)
-    const all = await surveysRepostitory.find()
+    const surveysRepository = getCustomRepository(SurveysRepository)
+    const all = await surveysRepository.find()
     return response.json(all)
   }
 
 }
 
-export { SurveysController }
+export { SurveysController };
